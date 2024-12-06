6caabf8cfa911632f922c9c81da6409c3c0c5112
Matthew Steeples <matthew@ledgerscope.com>
2024-12-06 01:48:27 +0000

    TIdy up formatting

--- Main.cs
+++ Main.cs
@@ -2,6 +2,7 @@
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
@@ -12,15 +13,19 @@ namespace DecoratorGenerator
     [Generator]
     public class Main : IIncrementalGenerator
     {
-        public void Initialize(IncrementalGeneratorInitializationContext context) {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
             var classDeclarations = context.SyntaxProvider.CreateSyntaxProvider(
                 predicate: static (s, _) => IsSyntaxTargetForGeneration(s),
                 transform: static (ctx, _) => GetSemanticTargetForGeneration(ctx))
             .Where(static m => m is not null);
 
-            static bool IsSyntaxTargetForGeneration(SyntaxNode node) {
-                if (node is InterfaceDeclarationSyntax m) {
-                    if (m.AttributeLists.Any(a => a.Attributes.Any(b => b.Name.ToString() == "Decorate"))) {
+            static bool IsSyntaxTargetForGeneration(SyntaxNode node)
+            {
+                if (node is InterfaceDeclarationSyntax m)
+                {
+                    if (m.AttributeLists.Any(a => a.Attributes.Any(b => b.Name.ToString() == "Decorate")))
+                    {
                         return true;
                     }
                     //=> node is InterfaceDeclarationSyntax m && m.AttributeLists.Any(a => a.Attributes.Any(b => b.Name.ToString() == nameof(DecorateAttribute)));
@@ -28,28 +33,32 @@ namespace DecoratorGenerator
                 return false;
             }
 
-            static ClassDeclarationSyntax GetSemanticTargetForGeneration(GeneratorSyntaxContext context) {
+            static NamespaceDeclarationSyntax GetSemanticTargetForGeneration(GeneratorSyntaxContext context)
+            {
                 var interfaceDeclarationSyntax = (InterfaceDeclarationSyntax)context.Node;
-                return OutputGenerator.GenerateOutputs(context.SemanticModel.GetDeclaredSymbol(interfaceDeclarationSyntax) as INamedTypeSymbol);
+                return OutputGenerator.GenerateOutputs(context.SemanticModel.GetDeclaredSymbol(interfaceDeclarationSyntax));
             }
 
-            IncrementalValueProvider<(Compilation, ImmutableArray<ClassDeclarationSyntax>)> compilationAndClasses
+            IncrementalValueProvider<(Compilation, ImmutableArray<NamespaceDeclarationSyntax>)> compilationAndClasses
                 = context.CompilationProvider.Combine(classDeclarations.Collect());
 
             context.RegisterSourceOutput(compilationAndClasses,
                 static (spc, source) => Execute(source.Item1, source.Item2, spc));
         }
 
-        private static void Execute(Compilation compilation, ImmutableArray<ClassDeclarationSyntax> classes, SourceProductionContext context) {
-            if (classes.IsDefaultOrEmpty) {
+        private static void Execute(Compilation compilation, ImmutableArray<NamespaceDeclarationSyntax> classes, SourceProductionContext context)
+        {
+            if (classes.IsDefaultOrEmpty)
+            {
                 // nothing to do yet
                 return;
             }
 
-            IEnumerable<ClassDeclarationSyntax> distinctClasses = classes.Distinct();
+            var distinctClasses = classes.Distinct();
 
-            foreach (var classDeclaration in distinctClasses) {
-                context.AddSource("Decorator." + classDeclaration.Identifier.ToString() + ".g.cs", SourceText.From(classDeclaration.NormalizeWhitespace().ToFullString(), Encoding.UTF8));
+            foreach (var namespaceDeclaration in distinctClasses)
+            {
+                context.AddSource("Decorator." + namespaceDeclaration.ChildNodes().OfType<ClassDeclarationSyntax>().First().Identifier.ToString() + ".g.cs", SourceText.From(namespaceDeclaration.NormalizeWhitespace().ToFullString(), Encoding.UTF8));
             }
 
             //var p = new Parser(compilation, context.ReportDiagnostic, context.CancellationToken);
--- OutputGenerator.cs
+++ OutputGenerator.cs
@@ -8,7 +8,8 @@ namespace DecoratorGenerator
 {
     internal static class OutputGenerator
     {
-        public static ClassDeclarationSyntax GenerateOutputs(INamedTypeSymbol type) {
+        public static NamespaceDeclarationSyntax GenerateOutputs(INamedTypeSymbol type)
+        {
             var interfaceName = type.Name;
             var className = type.Name.Substring(1);
             var decoratorName = className + "Decorator";
@@ -25,22 +26,23 @@ namespace DecoratorGenerator
             var modifier = SyntaxFacts.GetText(type.DeclaredAccessibility);
             var accessibilityModifier = SyntaxFactory.Token(SyntaxFacts.GetKeywordKind(modifier));
 
-            var classDeclaration = SyntaxFactory.ClassDeclaration(decoratorName)
+            return SyntaxFactory.NamespaceDeclaration(SyntaxFactory.ParseName(type.ContainingNamespace.ToDisplayString()))
+                .AddMembers(SyntaxFactory.ClassDeclaration(decoratorName)
                 .AddModifiers(accessibilityModifier, SyntaxFactory.Token(SyntaxKind.AbstractKeyword), SyntaxFactory.Token(SyntaxKind.PartialKeyword))
                 .AddBaseListTypes(SyntaxFactory.SimpleBaseType(parsedType))
                 .AddMembers(getField(parsedType, fieldName))
                 .AddMembers(getConstructor(decoratorName, constructorParamName, parsedType, fieldName))
-                .AddMembers(getMembers(type, fieldName).ToArray());
-
-            return classDeclaration;
+                .AddMembers(getMembers(type, fieldName).ToArray()));
         }
 
-        private static FieldDeclarationSyntax getField(TypeSyntax typeSyntax, string fieldName) {
+        private static FieldDeclarationSyntax getField(TypeSyntax typeSyntax, string fieldName)
+        {
             return SyntaxFactory.FieldDeclaration(SyntaxFactory.VariableDeclaration(typeSyntax)
                     .AddVariables(SyntaxFactory.VariableDeclarator(fieldName))).AddModifiers(SyntaxFactory.Token(SyntaxKind.PrivateKeyword), SyntaxFactory.Token(SyntaxKind.ReadOnlyKeyword));
         }
 
-        private static ConstructorDeclarationSyntax getConstructor(string decoratorName, string constructorParamName, TypeSyntax typeSyntax, string fieldName) {
+        private static ConstructorDeclarationSyntax getConstructor(string decoratorName, string constructorParamName, TypeSyntax typeSyntax, string fieldName)
+        {
             return SyntaxFactory.ConstructorDeclaration(decoratorName)
                     .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
                     .AddParameterListParameters(SyntaxFactory.Parameter(SyntaxFactory.Identifier(constructorParamName)).WithType(typeSyntax))
@@ -51,20 +53,26 @@ namespace DecoratorGenerator
                                     SyntaxKind.SimpleAssignmentExpression, SyntaxFactory.IdentifierName(fieldName), SyntaxFactory.IdentifierName(constructorParamName)))));
         }
 
-        private static IEnumerable<MemberDeclarationSyntax> getMembers(INamedTypeSymbol type, string fieldName) {
+        private static IEnumerable<MemberDeclarationSyntax> getMembers(INamedTypeSymbol type, string fieldName)
+        {
             var members = type.GetAllInterfaceMembers();
 
-            foreach (var member in members) {
-                if (member is IMethodSymbol {MethodKind: MethodKind.Ordinary } method) {
+            foreach (var member in members)
+            {
+                if (member is IMethodSymbol { MethodKind: MethodKind.Ordinary } method)
+                {
 
                     var methodCall = SyntaxFactory.InvocationExpression(
                         SyntaxFactory.MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression, SyntaxFactory.IdentifierName(fieldName), SyntaxFactory.IdentifierName(method.Name)))
                         .AddArgumentListArguments(method.Parameters.Select(p => SyntaxFactory.Argument(SyntaxFactory.IdentifierName(p.Name))).ToArray());
 
                     StatementSyntax statement;
-                    if (method.ReturnType.SpecialType == SpecialType.System_Void) {
+                    if (method.ReturnType.SpecialType == SpecialType.System_Void)
+                    {
                         statement = SyntaxFactory.ExpressionStatement(methodCall);
-                    } else {
+                    }
+                    else
+                    {
                         statement = SyntaxFactory.ReturnStatement(methodCall);
                     }
 
@@ -73,8 +81,10 @@ namespace DecoratorGenerator
                         .AddParameterListParameters(method.Parameters.Select(p => SyntaxFactory.Parameter(SyntaxFactory.Identifier(p.Name)).WithType(p.Type.ToTypeSyntax())).ToArray())
                         .WithBody(SyntaxFactory.Block(statement));
 
-                } else if (member is IPropertySymbol property) {
-                    
+                }
+                else if (member is IPropertySymbol property)
+                {
+
                     yield return SyntaxFactory.PropertyDeclaration(property.Type.ToTypeSyntax(), property.Name)
                         .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword), SyntaxFactory.Token(SyntaxKind.VirtualKeyword))
                         .AddAccessorListAccessors(
